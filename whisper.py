b1f7ffb5638d118073cc6607b12418fe489e1d11
maiken_dev <mikepengxinwei@gmail.com>
2024-02-17 03:22:12 -0500

    implemented transcribe

--- whisper.py
+++ whisper.py
@@ -2,6 +2,12 @@ import requests
 
 endpoint_url = "http://localhost:9000"
 
-response = requests.post(endpoint_url,)
 
-print(response)
+def transcribe(file_path, target_lang):
+    with open(file_path, "rb") as infile:
+        files = {"audio_file": infile}
+        response = requests.post(f"{endpoint_url}/asr?task=transcribe&language={target_lang}&output=json", files=files)
+        return response.json()["text"]
+
+
+print(transcribe("input.mp3", "en"))
