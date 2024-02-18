0dd32166bbfe8565364f931a0b2899ff16edf24d
maiken_dev <mikepengxinwei@gmail.com>
2024-02-17 22:46:00 -0500

    Implemented live logging

--- listener.py
+++ listener.py
@@ -1,18 +1,18 @@
 import sounddevice
 from scipy.io.wavfile import write
+from threading import Thread
+from time import time
 
 
-def record() -> None:
+def record(second: time) -> None:
     # sample_rate
     fs = 44100
 
-    # Asking to enter the recording time
-    second = int(input("Enter the Recording Time in seonds: "))
     print("Recording.....\n")
 
-    # Record the voice
-    record_voice = sounddevice .rec(int(second * fs), samplerate=fs, channels=2)
+    # Record the voices
+    record_voice = sounddevice.rec(int(second * fs), samplerate=fs, channels=2)
     sounddevice.wait()
-    write("recorded.wav", fs, record_voice)
+    Thread(target=write, args=["recorded.wav", fs, record_voice]).start()
 
     print("Recording is done please check your folder to listen to the recording")
--- main.py
+++ main.py
@@ -1,25 +1,77 @@
 from whisper import transcribe
 from listener import record
+from collections import deque
+from threading import Thread
 from time import time
-from time import sleep
 
 
-def run(printing_interval: time, translating_language: str):
-    # todo: allow user input here
-    record()
+class Handler:
+    """
 
-    # below is sample mp3
-    transcribed = transcribe('recorded.wav', translating_language)
+    """
+    queue: deque
+    thread: Thread
+    recording_interval: time
+    language: str
+    active: bool
 
-    for sentence in transcribed.split('.'):
-        printing = sentence.strip()
+    def __init__(self, language: str) -> None:
+        self.queue = deque()
+        self.thread = None
+        self.recording_interval = 4
+        self.language = language
+        self.active = False
 
-        if len(printing) > 0 and printing[-1] != '.':
-            print(printing + '.')
-        else:
-            print(printing)
-        sleep(printing_interval)
+    def capture(self) -> None:
+        # below is sample mp3
+        transcribed = transcribe('recorded.wav', self.language)
+        for sentence in transcribed.split('.'):
+            printing = sentence.strip()
+
+            if len(printing) > 0 and printing[-1] != '.':
+                self.queue.append(printing + '.')
+            else:
+                self.queue.append(printing)
+
+    def run(self) -> None:
+        while self.active:
+            self.thread = Thread(target=self.capture)
+            # records input audio and creates a mp3
+            record(self.recording_interval)
+            self.thread.start()
+        print("services halted.")
+
+    def dequeue(self) -> str:
+        if len(self.queue) > 0:
+            return self.queue.popleft()
+        return ""
 
 
 if __name__ == "__main__":
-    run(1, 'ja')
+    import keyboard
+
+    caption = ""
+
+    primary_engine = Handler("en")
+    primary_engine.active = True
+
+    running = True
+
+    main_thread = Thread(target=primary_engine.run)
+    try:
+        main_thread.start()
+        while running:
+            if keyboard.is_pressed("s"):
+                running = False
+                primary_engine.active = False
+
+                if primary_engine.thread.is_alive():
+                    primary_engine.thread.join()
+
+                if main_thread.is_alive():
+                    main_thread.join()
+
+            caption += primary_engine.dequeue()
+    finally:
+        print(primary_engine.queue)
+        print(caption)
